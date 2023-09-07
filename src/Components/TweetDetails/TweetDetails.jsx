e37d010ffd171f20949afea156c0cc9e30a3dc4c
AlexYunan <alexandrosgiounan1@hotmail.com>
2023-09-07 10:55:52 +0300

    Created new Component for tweet details

---
+++ src/Components/TweetDetails/TweetDetails.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const TweetDetails = () => {
+  return (
+    <div>
+      <React.Fragment>
+        <section className={`z-50 flex items-center sticky top-0 bg-opacity-95`}></section>
+      </React.Fragment>
+    </div>
+  );
+};
+
+export default TweetDetails;
