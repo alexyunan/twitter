6c189834f557794eed3245379269d6b73f58a403
AlexYunan <alexandrosgiounan1@hotmail.com>
2023-09-11 13:41:55 +0300

    added close and icon buttons from mui

--- src/Components/Profile/ProfileModal.jsx
+++ src/Components/Profile/ProfileModal.jsx
@@ -3,18 +3,21 @@ import Box from "@mui/material/Box";
 import Button from "@mui/material/Button";
 import Modal from "@mui/material/Modal";
 import { useFormik } from "formik";
+import { IconButton } from "@mui/material";
+import CloseIcon from "@mui/icons-material/Close";
 
 const style = {
   position: "absolute",
   top: "50%",
   left: "50%",
   transform: "translate(-50%, -50%)",
-  width: 400,
+  width: 600,
   bgcolor: "background.paper",
   border: "none",
   boxShadow: 24,
   p: 4,
   outline: "none",
+  borderRadius: 4,
 };
 
 export default function ProfileModal() {
@@ -42,7 +45,28 @@ export default function ProfileModal() {
     <div>
       <Button onClick={handleOpen}>Open modal</Button>
       <Modal open={true} onClose={handleClose} aria-labelledby='modal-modal-title' aria-describedby='modal-modal-description'>
-        <Box sx={style}></Box>
+        <Box sx={style}>
+          <form onSubmit={formik.handleSubmit}>
+            <div className='flex items-center justify-between'>
+              <div className='flex items-center space-x-3'>
+                <IconButton onClick={handleClose} aria-label='delete'>
+                  <CloseIcon />
+                </IconButton>
+                <p>Edit Profile</p>
+              </div>
+              <Button type='submit'>Save</Button>
+            </div>
+            <div className='overflow-y-scroll overflow-x-hidden h-[80vh]'>
+              <div>
+                <div className='w-full'>
+                  <div className='relative'>
+                    <img className='w-full h-[12rem] object-cover object-center' src='https://cdn.pixabay.com/photo/2023/08/26/18/01/planet-8215532_640.png' alt='' />
+                  </div>
+                </div>
+              </div>
+            </div>
+          </form>
+        </Box>
       </Modal>
     </div>
   );
