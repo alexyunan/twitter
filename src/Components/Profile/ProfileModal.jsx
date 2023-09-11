e03d436e21aedde7b79f57690fd62bfa4c2596c0
AlexYunan <alexandrosgiounan1@hotmail.com>
2023-09-11 13:48:04 +0300

    handleImageChange function.

--- src/Components/Profile/ProfileModal.jsx
+++ src/Components/Profile/ProfileModal.jsx
@@ -22,6 +22,7 @@ const style = {
 
 export default function ProfileModal() {
   const [open, setOpen] = React.useState(false);
+  const [upLoading, setUploading] = React.useState(false);
   const handleOpen = () => setOpen(true);
   const handleClose = () => setOpen(false);
 
@@ -40,6 +41,14 @@ export default function ProfileModal() {
     },
     onsubmit: handleSubmit,
   });
+
+  const handleImageChange = (event) => {
+    setUploading(true);
+    const { name } = event.target;
+    const file = event.target.files[0];
+    formik.setFieldValue(name, file);
+    setUploading(false);
+  };
 
   return (
     <div>
@@ -61,6 +70,7 @@ export default function ProfileModal() {
                 <div className='w-full'>
                   <div className='relative'>
                     <img className='w-full h-[12rem] object-cover object-center' src='https://cdn.pixabay.com/photo/2023/08/26/18/01/planet-8215532_640.png' alt='' />
+                    <input type='file' className='absolute top-0 left-0 w-full opacity-0 cursor-pointer' onChange={handleImageChange} name='backgroundImage' />
                   </div>
                 </div>
               </div>
